4d85e16d68967305823f389338aeb76f4f0e3e65
Upgrayedd0408 <michaellowry89@gmail.com>
2024-05-05 14:49:21 -0700

    implemented benchmark function

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,14 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  
+  yield
+  
+  end_time = Time.now
+  
+  run_time = end_time - start_time
+  
+  return run_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
